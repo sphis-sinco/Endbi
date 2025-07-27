5828723c4dee3f053ac51bb59d3cbeb2aef86c84
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-27 09:39:28 -0500

    fucking around cuz why not LOL

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,22 @@ class Main extends Sprite
 	{
 		super();
 		FlxG.save.bind('Endbi', 'Sinco');
+
+		#if sys
+		trace(Sys.args());
+		final haxeVer:Int = Sys.command('haxe', ['--version']);
+		trace(haxeVer);
+		trace(Sys.cpuTime());
+		// trace(Sys.environment());
+		trace(Sys.getCwd());
+		trace(Sys.programPath());
+		trace(Sys.systemName());
+		trace(Sys.time());
+
+		if (haxeVer != 0)
+			trace('Non-haxer detected, posting nudes on twitter.com...');
+		#end
+
 		addChild(new FlxGame(0, 0, PlayState));
 	}
 }
