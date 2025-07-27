a04169fa491bf03037d3da765eafcf72fdb51f07
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-27 06:47:14 -0500

    player and opponent healths

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4,6 +4,9 @@ import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var PLAYER_HEALTH:Int = 1;
+	public var OPPONENT_HEALTH:Int = 1;
+
 	override public function create()
 	{
 		super.create();
