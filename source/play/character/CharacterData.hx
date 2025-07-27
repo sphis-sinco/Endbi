ed723dd9209d13727dd931e5ef489e77afb50be0
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-27 06:55:08 -0500

    have free will

--- source/play/character/CharacterData.hx
+++ source/play/character/CharacterData.hx
@@ -3,14 +3,14 @@ package play.character;
 typedef CharacterData =
 {
 	var assetFolder:String;
-	var assetNames:
+	var ?assetNames:
 		{
-			var idle:String;
-			var defence:String;
-			var death:String;
-			var atk1:String;
-			var atk2:String;
-			var atk3:String;
+			var ?idle:String;
+			var ?defence:String;
+			var ?death:String;
+			var ?atk1:String;
+			var ?atk2:String;
+			var ?atk3:String;
 		};
 
 	var attack1:CharacterAttackInformation;
