b4a7c905963b2f65a23169ab5b0c8810302df415
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-27 06:53:33 -0500

    asset names

--- source/play/character/CharacterData.hx
+++ source/play/character/CharacterData.hx
@@ -3,6 +3,15 @@ package play.character;
 typedef CharacterData =
 {
 	var assetFolder:String;
+	var assetNames:
+		{
+			var idle:String;
+			var defence:String;
+			var death:String;
+			var atk1:String;
+			var atk2:String;
+			var atk3:String;
+		};
 
 	var attack1:CharacterAttackInformation;
 	var attack2:CharacterAttackInformation;
